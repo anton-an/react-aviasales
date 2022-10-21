2ee59192dc4d0c1b2c29991bbfa8977d23e448d5
Anton A <purplecancer@yandex.ru>
2022-10-22 00:01:43 +0300

    Update ticketsSlice.js

--- src/reducers/ticketsSlice.js
+++ src/reducers/ticketsSlice.js
@@ -84,29 +84,16 @@ const ticketsSlice = createSlice({
 const filterTickets = (filters, tickets) => {
   const allTickets = JSON.parse(JSON.stringify(tickets))
   const filtered = []
-  if (filters.all.isActive) {
-    return allTickets
-  }
-  if (filters.noTransfers.isActive) {
-    filtered.push(
-      ...allTickets.filter((ticket) => ticket.segments.reduce((acc, segment) => acc + segment.stops.length, 0) === 0)
-    )
-  }
-  if (filters.one.isActive) {
-    filtered.push(
-      ...allTickets.filter((ticket) => ticket.segments.reduce((acc, segment) => acc + segment.stops.length, 0) === 1)
-    )
-  }
-  if (filters.two.isActive) {
-    filtered.push(
-      ...allTickets.filter((ticket) => ticket.segments.reduce((acc, segment) => acc + segment.stops.length, 0) === 2)
-    )
-  }
-  if (filters.three.isActive) {
-    filtered.push(
-      ...allTickets.filter((ticket) => ticket.segments.reduce((acc, segment) => acc + segment.stops.length, 0) === 3)
-    )
-  }
+  if (filters.all.isActive) return tickets
+
+  if (filters.noTransfers.isActive)
+    filtered.push(...allTickets.filter((ticket) => ticket.segments.every((segment) => segment.stops.length === 0)))
+  if (filters.one.isActive)
+    filtered.push(...allTickets.filter((ticket) => ticket.segments.every((segment) => segment.stops.length === 1)))
+  if (filters.two.isActive)
+    filtered.push(...allTickets.filter((ticket) => ticket.segments.every((segment) => segment.stops.length === 2)))
+  if (filters.three.isActive)
+    filtered.push(...allTickets.filter((ticket) => ticket.segments.every((segment) => segment.stops.length === 3)))
   return filtered
 }
 
